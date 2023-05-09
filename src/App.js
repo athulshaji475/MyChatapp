616e2d99450cb9abd43a29e9e65381db6c067c42
AthulShaji <athulshaji475@gmail.com>
2023-05-09 13:16:53 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import {ChatEngine} from 'react-chat-engine'
+import axios from 'axios'
+import  './App.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    <ChatEngine
+        height="100vh"
+        projectID=""
+        userName=""
+        userSecret=""
+    />
+  )
 }
 
-export default App;
+export default App
