de2f2518572746589c1993a4e379d4e3ae70ec5e
AthulShaji <athulshaji475@gmail.com>
2023-05-10 17:25:22 +0530

    for message test

--- src/component/MessageForm.jsx
+++ src/component/MessageForm.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import { useState } from 'react'
+import { sendMessage ,isTyping} from 'react-chat-engine'
 
-function MessageForm() {
+
+function MessageForm(props) {
+const [value, setvalue] = useState('')
+const {chatId,creds}=props
+
+//on submit-------------------------------
+    const handleSubmit=(e)=>{
+     e.preventDefault();
+     const text=value.trim()
+     if(text.length>0) sendMessage(creds,chatId,{text})
+     
+     
+     }
+
+//on change----------------------------------
+       const handleChange=(e)=>
+        {
+          setvalue(e.target.value)
+          isTyping(props,chatId)
+
+        }
+
+
   return (
-    <div>
-      my Message
-    </div>
+    <form className='message-form' onSubmit={handleSubmit}>
+     <input 
+      className='message-input'
+      placeholder='Send a message...'
+      value={value}
+      onChange={handleChange}
+    />
+    </form>
   )
 }
 
